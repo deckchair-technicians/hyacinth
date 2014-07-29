ec987170253aa58bd7a39a2c84723d18a7a363df
Matt Savage <matthew.savage@gmail.com>
2014-07-29 14:53:45 +0100

    Removing Amazonica implementation- slow, prone to out of memory, massive dependencies buggy. Who would have thought- the command line is better.

--- project.clj
+++ project.clj
@@ -15,7 +15,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
                  [cheshire "5.3.0"]
-                 [amazonica "0.1.28"]
                  [clj-ssh "0.5.9"]
                  [me.raynes/fs "1.4.5"]]
 
