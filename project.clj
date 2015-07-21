96f1fbfa13887bd736fabdb793888fa996c0e605
David Williams <dpw341@gmail.com>
2015-07-21 07:06:47 +0100

    Update repos for theorem

--- project.clj
+++ project.clj
@@ -2,22 +2,24 @@
 
   :description "Bucket abstraction over S3, FTP, file, memory map"
 
-  :url "https://bitbucket.org/theshoreditchproject/hyacinth"
-
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://bitbucket.org/TheoremTechnology/hyacinth"
 
   :plugins [[s3-wagon-private "1.1.2"]
             [lein-set-version "0.3.0" ]]
 
-  :repositories [["callisto" "s3p://callisto-artifacts/releases/"]]
+  :deploy-repositories [["releases" {:url "s3p://repo.solo.com/releases" :creds :gpg}]
+                        ["snapshots" {:url "s3p://repo.solo.com/snapshots" :creds :gpg}]]
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :repositories [["solo" ~(if (System/getenv "AWS_ACCESS_KEY_ID")
+                            {:url "s3p://repo.solo.com/releases" :username :env :passphrase :env}
+                            {:url "s3p://repo.solo.com/releases" :creds :gpg})]]
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.6.0"]
                  [cheshire "5.3.0"]
                  [clj-ssh "0.5.9"]
                  [me.raynes/fs "1.4.5"]]
 
-  :profiles {:dev {:dependencies [[midje "1.6.2"]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]
                                   [midje-junit-formatter "0.1.0-SNAPSHOT"]]
                    :plugins [[lein-midje "3.1.0"]]}})
