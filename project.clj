5fd6c06cbcd3f2dda955dfc7fd68fe2f8d6ee7ca
David Williams <david.williams@theorem.technology>
2015-09-15 15:27:43 +0100

    more boilerplate for deployment

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject technology.theorem/hyacinth "DEV"
 
+
   :repositories [["theorem-repo" ~(if (System/getenv "AWS_ACCESS_KEY_ID")
                                     {:url "s3p://theorem-repo/repo" :username :env :passphrase :env}
                                     {:url "s3p://theorem-repo/repo" :creds :gpg})]]
 
   :description "Bucket abstraction over S3, FTP, file, memory map"
 
-  :plugins [[s3-wagon-private "1.1.2"]
+  :plugins [[lein-maven-s3-wagon "0.2.4"]
             [lein-set-version "0.3.0" ]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
